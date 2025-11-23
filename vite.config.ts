67910b3891dd105a83a85d56f68f24e4afc32b5b
xujh <>
2025-11-23 22:38:17 +0800

    chore: 补充图片资源

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  viteStaticCopy({
+    targets: [
+      {
+        src: 'assets/images/*',
+        dest: 'assets/images'
+      }
+    ]
+  })
+  ],
   base: './', // Ensures relative paths for assets on GitHub Pages
   build: {
     outDir: 'dist',
